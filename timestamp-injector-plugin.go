e63274f96d30829264e394147eced89cebc3aed4
rhys-uk <rhys@clement-evans.co.uk>
2026-05-14 16:43:09 +0100

    Update timestamp-injector-plugin.go inserted time value
    switching from milliseconds to microseconds. Both are supported by New Relic

--- timestamp-injector-plugin.go
+++ timestamp-injector-plugin.go
@@ -1,40 +1,40 @@
 package timestamp_injector
 
 import (
-	"context"
-	"net/http"
-	"strconv"
-	"time"
+        "context"
+        "net/http"
+        "strconv"
+        "time"
 )
 
 // Config holds plugin configuration.
 type Config struct {
-	HeaderName string `json:"headerName,omitempty"`
+        HeaderName string `json:"headerName,omitempty"`
 }
 
 // CreateConfig creates default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{
-		HeaderName: "X-Queue-Start", // default value optimized for APM queue time
-	}
+        return &Config{
+                HeaderName: "X-Queue-Start", // default value optimized for APM queue time
+        }
 }
 
 type TimestampInjector struct {
-	name       string
-	next       http.Handler
-	headerName string
+        name       string
+        next       http.Handler
+        headerName string
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	return &TimestampInjector{
-		name:       name,
-		next:       next,
-		headerName: config.HeaderName,
-	}, nil
+        return &TimestampInjector{
+                name:       name,
+                next:       next,
+                headerName: config.HeaderName,
+        }, nil
 }
 
 func (eh *TimestampInjector) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	msec := time.Now().UnixMilli()
-	req.Header.Set(eh.headerName, strconv.FormatInt(msec, 10))
-	eh.next.ServeHTTP(rw, req)
+        usec := time.Now().UnixMicro()
+        req.Header.Set(eh.headerName, strconv.FormatInt(usec, 10))
+        eh.next.ServeHTTP(rw, req)
 }
